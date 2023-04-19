26260fe9571fd36d0881cd6bfbb3914fa546da72
Katy Barrus <131273471+kbarrus27@users.noreply.github.com>
2023-04-19 15:11:45 -0400

    Update Rubiks_Cube_Scramble_generator.py
    Cleaned code -- got rid of extraneous comments and simplified the move-combining code. Fixed a bug where the code designed to create a "2" move was editing lastMove and not the sequence itself.

--- Rubiks_Cube_Scramble_generator.py
+++ Rubiks_Cube_Scramble_generator.py
@@ -1,5 +1,4 @@
 #Rubik's Cube Scramble generator
-#12/18/2020 -- Katy Barrus
 
 import random
 
@@ -7,107 +6,32 @@ moves = ['R', 'L', 'U', 'B', 'F', 'D', "R'", "L'", "U'", "B'", "F'", "D'"]
 numberOfMoves = 25
 sequence = []
 
-'''
-print(str(len(sequence)))
-sequence += 'R'
-print(str(len(sequence)))
-print(sequence)
-print(len(sequence) < numberOfMoves)
-
-nextMove = moves[random.randint(0,11)]
-lastMove = sequence[len(sequence)-1]
-    
-print("Last move: " + lastMove)
-print("Next move: " + nextMove)
-'''
-
 while len(sequence) < numberOfMoves:
-    
-    #update variables
+
+    #Generate a move
     nextMove = moves[random.randint(0,11)]
-    #print("Next move: " + nextMove)
         
-        
-    #combine the same moves
-    if len(sequence) > 1:
-               
-        lastMove = sequence[len(sequence)-1]
-        #print("Last move: " + lastMove)
-                
-        if nextMove == lastMove:
-            #print("These two moves are the same.")
-            #print("Removing " + lastMove + "...")
-            sequence.remove(lastMove)
-        
-            if nextMove in ['R', 'L', 'U', 'B', 'F', 'D']:
-                #print("Changing " + nextMove + " to " + nextMove + "2...")
-                nextMove = nextMove + "2"
-            
-            elif nextMove in ["R'", "L'", "U'", "B'", "F'", "D'"]:
-                #print("Changing " + nextMove + "...")
-                nextMove = nextMove.replace("'", "")
-                nextMove = nextMove + "2"
-                #print("The move is now " + nextMove + ".")
-            
-            else:
-                print("Error -- move is not in list of moves")
-        
-        #if you have a "2" version of one move and then the same move after or before it
-        elif nextMove in lastMove:
-            #print("need to generate a new move")
-            pass
-
-        #if you have one move, and then an inverse of the same move
-        elif nextMove == lastMove + "'":
-            #print("need to generate a new move")
-            pass
-
-        elif nextMove + "'" == lastMove:
-            #print("need to generate a new move")
-            pass
-
-        else:
-            #add the next move
-            sequence.append(nextMove)
-    else:
+    #If this is the first move, add it to the list
+    if len(sequence) < 1:
         sequence.append(nextMove)
     
-    #print("Here is your current list of moves: ")
-    #print(sequence)
-    
+    #If this isn't the first move:
+    else:
+        lastMove = sequence[len(sequence)-1]
+       
+        #Check to see if this and the last move are same "type" of move, like R and R' and R2
+        if nextMove[0] == lastMove[0]: 
+            
+            #If lastMove and nextMove are the same (two Rs or two R's), change the prior move to its "2" version (R2)
+            if nextMove == lastMove:
+                sequence[len(sequence)-1] = lastMove[0] + '2'
+
+            #Otherwise, don't do anything -- the program will pick a new move on its next loop
+        
+        #If this move and the last move are different "types" of moves, like R and L, add this move to the sequence
+        else:
+            sequence.append(nextMove)
+
 print("Here is your final list of moves: ")
 for letter in sequence:
     print(letter, end = "   ")
-
-#Code to come back to later
-'''
-#check whether there are any moves that cancel each other out
-for letter in ['R', 'L', 'U', 'B', 'F', 'D']:
-if (letter in lastMove) and (letter in nextMove):
-    
-    #if you have the same move twice
-    if nextMove == lastMove:
-        print("These two moves are the same.")
-        print("Removing " + lastMove + "...")
-        sequence.remove(lastMove)
-
-        if nextMove in ['R', 'L', 'U', 'B', 'F', 'D']:
-            print("Changing " + nextMove + " to " + nextMove + "2...")
-            nextMove = nextMove + "2"
-    
-        elif nextMove in ["R'", "L'", "U'", "B'", "F'", "D'"]:
-            print("Changing " + nextMove + "...")
-            nextMove = nextMove.replace("'", "")
-            nextMove = nextMove + "2"
-            print("The move is now " + nextMove + ".")
-    
-        else:
-            print("Error -- move is not in list of moves")
-        
-    #Other scenarios:
-    #if you have a "2" version of one move and then the same move after or before it
-    #if you have one move, and then an inverse of the same move
-
-    #In both cases, generate a new move    
-    else:
-    '''
